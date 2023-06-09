e43c1b3f12b665c0e7a450a7f6add6879b2dc2a9
‘mlt’ <mltapp@163.com>
2023-06-09 11:47:39 +0800

    vant集成

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,27 @@
+/*
+ * @Author: ‘mlt’ ‘mltapp@163.com’
+ * @Date: 2023-06-09 11:20:12
+ * @LastEditors: ‘mlt’ ‘mltapp@163.com’
+ * @LastEditTime: 2023-06-09 11:45:30
+ * @FilePath: \vue-cli-electron-template-electron11\vue.config.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 const path = require('path')
 const IS_PROD = process.env.NODE_ENV === 'production'
-
+const { VantResolver } = require('unplugin-vue-components/resolvers')
+const ComponentsPlugin = require('unplugin-vue-components/webpack')
 function resolve (dir) {
   return path.join(__dirname, dir)
 }
 
 module.exports = {
+  configureWebpack: {
+    plugins: [
+      ComponentsPlugin({
+        resolvers: [VantResolver()]
+      })
+    ]
+  },
   pages: {
     app: {
       title: 'vue-cli-electron-template',
